4b887740ea13220d84f7ce0de25c46bc263888e8
xial <1169794338@qq.com>
2020-12-13 15:48:13 +0800

    init

--- js/watch.js
+++ js/watch.js
@@ -1,5 +1,5 @@
 function process (el, id) {
-    Common.muipost("/app/common/online", { online: el, userId: id }, function (data) {
+    Common.muipost("/common/online", { online: el, userId: id }, function (data) {
        // console.log(JSON.stringify(data))
     }, function () {})
 }
